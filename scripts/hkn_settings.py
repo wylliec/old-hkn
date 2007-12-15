8d1945ff042f72b600977c666ef7c2c55f037212
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2007-12-15 13:31:44 +0000

    Changed so that it no longer hardcodes the path to the hkn.settings module

--- scripts/hkn_settings.py
+++ scripts/hkn_settings.py
@@ -1,7 +1,10 @@
 from django.core.management import setup_environ
 import django, sys
 
-sys.path.append("/home/hzarka")
-
+# import settings module from one directory down
+sys.path.append("../..")
 import hkn.settings
+sys.path.remove("../..")
+
 setup_environ(hkn.settings)
+
