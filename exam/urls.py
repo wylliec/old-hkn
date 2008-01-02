222b6122f9843642a9b53ff60abad00817fb2b3b
Chris Lay <clay@hkn.eecs.berkeley.edu>
2008-01-02 10:27:55 +0000

    clay: added rough draft of exam files pages

---
+++ exam/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls.defaults import *
+from hkn.settings import SERVER_ROOT, IMAGES_PATH
+
+urlpatterns = patterns('',
+    # Example:
+
+    # main page
+    (r'^$', 'hkn.exam.views.main'),
+	(r'^faq/$', 'hkn.exam.views.faq'),
+	(r'^committee/$', 'hkn.exam.views.committee'),
+	(r'^onlineexams/$', 'hkn.exam.views.onlineexams'),
+	(r'^submit/$', 'hkn.exam.views.submit'),
+)
