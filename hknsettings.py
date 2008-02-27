9ee9f74bb6fe6eae961a3127b7e9a3c66892a3b5
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-02-27 06:01:20 +0000

    Accidentally committed...

--- hknsettings.py
+++ hknsettings.py
@@ -1,7 +1,5 @@
 # a valid value should look something like this:
-SERVER_ROOT = "/home/hzarka/hkn/hkn/"
+## SERVER_ROOT = "/home/hzarka/website/hkn/"
 
-# SERVER_ROOT = "/dev/null"
-
-FORCE_LOGIN = True
+SERVER_ROOT = "/dev/null"
 
