a5fa76c3c9ca9cec8459a2cc6b0bb7a539593f45
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-03-07 03:11:45 +0000

    Added reset for exams app

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -3,6 +3,7 @@
 ../manage.py reset --noinput cand
 ../manage.py reset --noinput course
 ../manage.py reset --noinput event
+../manage.py reset --noinput exam
 ../manage.py reset --noinput info
 ../manage.py reset --noinput request
 ../manage.py reset --noinput tutor
