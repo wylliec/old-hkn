1dcaf8b7531e326910d5d64d5b10e303bd45605c
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-06 00:33:09 +0000

    Added event creation, modification, and deletion as default permissions for officers

--- scripts/create_permissions.py
+++ scripts/create_permissions.py
@@ -6,7 +6,7 @@ default_permissions = {
 "everyone" : ("main.hkn_everyone", "info.view_officers"),
 "candidates" : ("main.hkn_candidate", "main.hkn_candidate_plus", "info.view_officers", "info.view_candidates"),
 "members" : ("main.hkn_member", "main.hkn_member_plus", "main.hkn_candidate_plus", "info.view_officers", "info.view_members"),
-"officers" : ("main.hkn_officer", "info.view_candidates"),
+"officers" : ("main.hkn_officer", "info.view_candidates", "event.add_event", "event.change_event", "event.delete_event"),
 "current_officers" : ("main.hkn_current_officer",),
 "group_vp" : ("info.view_restricted",)
 }
