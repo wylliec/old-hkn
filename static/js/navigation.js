98df5e8e59d591fa028e79f783894f1f1261a82a
Arjun Singh <arjun@hkn.eecs.berkeley.edu>
2008-08-06 04:46:49 +0000

    fixes #1
    also testing the post-commit hook

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -5,7 +5,7 @@
  * Finds what path we're at currently and extracts the lowest subdirectory.
  * It will then look to see if there is a toplevel menu that has that name. If so,
  * it will make it selected. If not it will pick the first one it finds and make that 
- * selected. Might be deprecated soon.
+ * selected. Might be deprecated soon.  
  */
 $(document).ready(function() {
 	current = location.pathname.replace('/','');
