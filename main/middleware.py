613d9bc326b8fec11d84fef4f67d6d0d86977581
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-06 00:24:36 +0000

    New switch_user option which is a persisted version of as_user

--- main/middleware.py
+++ main/middleware.py
@@ -39,6 +39,16 @@ class HknAuthMiddleware(object):
             user = get_object_or_404(User, username=as_user)
             request.user = user
 
+        if 'switch_user' in request.GET and (request.user.is_superuser or request.session.has_key('_switch_user')):
+            request.session['_switch_user'] = True
+            as_user = request.GET.get('switch_user')
+            rpc = request.GET.copy()
+            del rpc['switch_user']
+            request.GET = rpc
+            user = get_object_or_404(User, username=as_user)
+            request.session['_auth_user_id'] = user.id
+            request.user = user
+
         if FORCE_LOGIN and request.path.find("login") == -1 and request.user.is_anonymous():
             return HttpResponseRedirect("/login/?next=%s" % request.path)
         return None
