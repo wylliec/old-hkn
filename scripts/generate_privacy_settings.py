920df45b05aca4adf014f116daae8c8f4357c75e
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-15 20:02:36 +0000

    generate_privacy_settings.py now generates for all users, and load_fixtures calls the script after loading as there appear to be problems with serialization for PickleField

--- scripts/generate_privacy_settings.py
+++ scripts/generate_privacy_settings.py
@@ -6,7 +6,8 @@ from hkn.info.models import *
 from hkn.info.constants import MEMBER_TYPE
 
 def main():
-    for person in list(Person.candidates.all()):
+    for person in list(Person.objects.all()):
+    	person.privacy = {}
         person.privacy['email'] = random.choice((MEMBER_TYPE.CANDIDATE, MEMBER_TYPE.OFFICER))
         person.privacy['phone'] = random.choice((MEMBER_TYPE.CANDIDATE, MEMBER_TYPE.OFFICER, 1000))
         person.save()
--- scripts/load_fixtures.sh
+++ scripts/load_fixtures.sh
@@ -4,3 +4,5 @@ echo "Loading cached fixtures"
 echo "If this doesn't work, run rebuild_all.py instead"
 ./clear_db.py
 ../manage.py loaddata ../fixtures/all.json
+echo "Re-generating privacy settings"
+./generate_privacy_settings.py
