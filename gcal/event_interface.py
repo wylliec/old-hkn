1980574cab7d2861bcc4ff5ff3d998d2853a7c4a
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-16 01:03:21 +0000

    Interface now uses model signals instead of hooking into event's save() and delete() directly

---
+++ gcal/event_interface.py
@@ -0,0 +1,30 @@
+from django.db.models import signals
+from hkn.event.models import Event
+from hkn.gcal import utils
+
+try:
+    from hkn.settings import GCAL_ENABLED
+except:
+    GCAL_ENABLED = False
+
+def if_enabled(fn):
+    def check_enabled(*args, **kwargs):
+        if not GCAL_ENABLED:
+            return
+        fn(*args, **kwargs)
+    return check_enabled
+
+@if_enabled
+def event_saved(event):
+    if len(event.gcal_id) > 0:
+        event.gcal_id = utils.updateEvent(event)
+    else:
+        event.gcal_id = utils.addEvent(event)
+
+@if_enabled
+def event_deleted(event):
+    if len(event.gcal_id) > 0:
+        utils.deleteEvent(event)
+
+signals.pre_save.connect(event_saved, Event)
+signals.post_delete.connect(event_deleted, Event)
