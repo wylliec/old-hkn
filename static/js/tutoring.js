e533164ec7c69c50289159efe6def4a934fee26c
Ryan Zheng <rzheng@hkn.eecs.berkeley.edu>
2008-08-17 05:29:33 +0000

    I hope this fixes #43

--- static/js/tutoring.js
+++ static/js/tutoring.js
@@ -2,35 +2,90 @@ var color = "#6f6";
 var colorcur = "#fb6";
 
 var locked = false;
-var tutoringcss = document.createElement('style');
-tutoringcss.type = 'text/css';
-tutoringcss.rel = 'stylesheet';
-document.getElementsByTagName("head")[0].appendChild(tutoringcss);
+var highlighted = false;
+
+function getObjectCss () {
+	var css = null;
+	try {
+		var head = document.getElementsByTagName("head").item(0);
+		head.appendChild(document.createElement("style"));
+		css = document.styleSheets[document.styleSheets.length-1];
+	} catch (ex) {
+		css = document.createStyleSheet("tutoringStyle.css");
+	}
+	return css;
+}
+
+function addCssRule (css, selector, rule) {
+	if (css.insertRule) {
+		css.insertRule(selector + " { " + rule + " }", css.cssRules.length);
+	} else if(css.addRule) {
+		css.addRule(selector, rule);
+	}
+}
+
+function deleteCssRule (css, selector) {
+	if (!selector) {
+		return;
+	}
+
+	if (tutoringcss.cssRules) {
+		rules = tutoringcss.cssRules;
+	}
+	else if (tutoringcss.rules) {
+		rules = tutoringcss.rules;
+	}
+	else {
+		return;
+	}
+
+	for (var it = 0; it < rules.length; it++) {
+		if (rules[it].selectorText.toLowerCase() == selector.toLowerCase()) {
+			if (tutoringcss.deleteRule) {
+				tutoringcss.deleteRule(it);
+			}
+			else if (tutoringcss.removeRule) {
+				tutoringcss.removeRule(it);
+			}
+			return;
+		}
+	}
+}
 
 // Highlight using these funky colors.
-function highlight(className) {
-	if(!locked) tutoringcss.innerHTML = '.' + className + 'cur { background: ' + colorcur + ' !important; }' +
-				'.' + className + ' { background: ' + color + ' !important; }';
+function highlight (className) {
+	if (!locked) {
+		deleteCssRule(tutoringcss, '.' + highlighted + 'cur');
+		deleteCssRule(tutoringcss, '.' + highlighted);
+		highlighted = className;
+		addCssRule(tutoringcss, '.' + className + 'cur', 'background: ' + colorcur + ' !important');
+		addCssRule(tutoringcss, '.' + className, 'background: ' + color + ' !important');
+	}
 }
 
 // Get rid of the css.
-function unhighlight(className){
-	if(!locked) tutoringcss.innerHTML = "";
+function unhighlight (className) {
+	if (!locked) {
+		deleteCssRule(tutoringcss, '.' + highlighted + 'cur');
+		deleteCssRule(tutoringcss, '.' + highlighted);
+		clearTutoringRules();
+		highlighted = false;
+	}
 }
 
 // Highlight and stay highlighted until we click something else.
 // If you do click a different class, highlight it instead of clearing.
-function locklight(className){
+function locklight (className) {
 	if (!locked){
 		highlight(className);
 		locked = className;
 	}
-	else if(locked != className){
+	else if (locked != className) {
 		locked = false;
 		highlight(className);
 		locked = className;
 	}
-	else{
+	else {
 		locked = false;
 		highlight(className);
 	}
@@ -38,3 +93,4 @@ function locklight(className){
 	return false;
 }
 
+var tutoringcss = getObjectCss();
