6dd83b672f06f1740e6627afa6468d522c8c2f1d
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-17 09:35:53 +0000

    AlreadyRegistered exception rather than Exception

--- django-common/request/__init__.py
+++ django-common/request/__init__.py
@@ -1,10 +1,13 @@
 from request.constants import REQUEST_OBJECT_CONFIRM_ATTR, REQUEST_OBJECT_METAINFO_ATTR, REQUEST_OBJECT_COMMENT_ATTR
 
+class AlreadyRegistered(Exception):
+    pass
+
 registry = []
 
 def register(model, model_metainfo_function, confirmation_attr='confirm', comment_attr=None, request_confirmation_function_attr=None):
     if model in registry:
-        raise Exception('Model %s already registered for requests framework!' % model.__name__)
+        raise AlreadyRegistered('Model %s already registered for requests framework!' % model.__name__)
     registry.append(model)
 
     setattr(model, REQUEST_OBJECT_CONFIRM_ATTR, confirmation_attr)
