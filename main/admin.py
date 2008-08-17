cd7b76c594eb4df480c53d49f55ce4979eb9895f
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-17 08:32:16 +0000

    Integrated photologue into admin

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,16 @@
 from django.contrib.admin.sites import AdminSite
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin
+from photologue.admin import GalleryAdmin, PhotoAdmin, PhotoEffectAdmin, PhotoSizeAdmin, WatermarkAdmin
+from photologue.models import Gallery, GalleryUpload, PhotoEffect, PhotoSize, Watermark, Photo
 
 admin_site = AdminSite()
 admin_site.register(FlatPage, FlatPageAdmin)
+
+admin_site.register(Gallery, GalleryAdmin)
+admin_site.register(GalleryUpload)
+admin_site.register(Photo, PhotoAdmin)
+admin_site.register(PhotoEffect, PhotoEffectAdmin)
+admin_site.register(PhotoSize, PhotoSizeAdmin)
+admin_site.register(Watermark, WatermarkAdmin)
 
