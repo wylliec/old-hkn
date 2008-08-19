71faa19e8e007e04be691a8dde254534da4c0d42
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-19 18:37:00 +0000

    clean_pyc uses SERVER_ROOT now as the base

--- scripts/clean_pyc.py
+++ scripts/clean_pyc.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import os
+import hkn_settings
+from django.conf import settings
 
-cwd = os.getcwd()
-if cwd.endswith('scripts'):
-	cwd = cwd[:-7]
+cwd = settings.SERVER_ROOT
 for root, dirs, files in os.walk(cwd):
 	for file in files:
-		if file.endswith('pyc'):
+		if file.endswith('.pyc'):
 			print 'deleting', os.path.join(root, file)
 			os.remove(os.path.join(root, file))
 
