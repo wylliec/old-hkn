e2180855da34b464dc957ec82989ce13950a9810
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-21 06:17:26 +0000

    cached tutor and course fixtures as these rarely change. rebuild_all.py will load these fixtures by default rather than actually rebuilding

--- scripts/rebuild_all.py
+++ scripts/rebuild_all.py
@@ -14,6 +14,7 @@ call_command('loaddata', '../fixtures/flatpages.json')
 import create_positions; create_positions.main()
 import create_permissions; create_permissions.main()
 
+
 import import_people
 import_people.import_people("data/info-candidates-sp08.tsv", MEMBER_TYPE.CANDIDATE)
 import_people.import_people("data/info-people.tsv", MEMBER_TYPE.EXCANDIDATE)
@@ -24,11 +25,17 @@ import create_superusers; create_superusers.main()
 import set_initiates; set_initiates.main()
 
 print 'Importing events'
-#./import_events_from_webcal.py
 call_command('loaddata', '../fixtures/events.json')
-os.system("python run_generate_scripts.py")
-os.system("python run_course_scripts.py")
-os.system("python run_exam_scripts.py")
+#./import_events_from_webcal.py
 
+print 'Importing courses'
+call_command('loaddata', '../fixtures/course.json')
+#os.system("python run_course_scripts.py")
+
+print "Importing tutor data"
+call_command('loaddata', '../fixtures/tutor.json')
+#import import_tutor_data; import_tutor_data.main()
+
+os.system("python run_exam_scripts.py")
+os.system("python run_generate_scripts.py")
 import generate_review_problems; generate_review_problems.main()
-import import_tutor_data; import_tutor_data.main()
