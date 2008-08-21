47277071fc7731eb9a75e6bec7a8cd6244039013
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-21 23:00:31 +0000

    Added CachingManager

--- django-common/nice_types/db.py
+++ django-common/nice_types/db.py
@@ -17,3 +17,21 @@ class QuerySetManager(models.Manager):
     def get_query_set(self):
         return self.model.QuerySet(self.model)
 
+class CachingManager(models.Manager):
+    _cache = {}
+    def get_cached(self, **kwargs):
+        cls = self.__class__
+        key = cls.get_cache_key(**kwargs)
+        try:
+            return cls._cache[key]
+        except KeyError:
+            cls._cache[key] = cls.get_cache_value( self.get_query_set().get(**kwargs) )
+            return cls._cache[key]
+    
+    @classmethod    
+    def clear_cache(cls):
+        cls._cache.clear()
+        
+    @staticmethod
+    def get_cache_value(instance):
+        return instance        
