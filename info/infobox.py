6e9099cdfcfe94175f178c17140080a02982afae
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-22 03:45:34 +0000

    forgot to commit

--- info/infobox.py
+++ info/infobox.py
@@ -1,15 +1,20 @@
+from django.template.loader import render_to_string
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext 
 from hkn.info.models import Person
 
-def tutor(request, person_id):
-    p = get_object_or_404(Person, pk = person_id)
-    assignments = p.assignment_set.all().latest_version()
-    courses_tutored = ", ".join([ct.course.short_name() for ct in p.cantutor_set.for_current_semester().order_by('course__department_abbr', 'course__number')])
+def tutor(request, person_id, return_string=False, person=None):
+    if not person:
+        person = get_object_or_404(Person, pk = person_id)
+    assignments = person.assignment_set.all().latest_version()
+    courses_tutored = ", ".join([ct.course.short_name() for ct in person.cantutor_set.for_current_semester().order_by('course__department_abbr', 'course__number')])
 
     d = {}
-    d["person"] = p
+    d["person"] = person
     d["assignments"] = assignments
     d["courses"] = courses_tutored
     
-    return render_to_response("info/infobox/tutor.html", d, context_instance=RequestContext(request))
+    if return_string:
+        return render_to_string("info/infobox/tutor.html", d, context_instance=RequestContext(request))
+    else:
+        return render_to_response("info/infobox/tutor.html", d, context_instance=RequestContext(request))
