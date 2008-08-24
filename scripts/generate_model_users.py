59f9f01f2e7123a0d298fbcd2419036b8d185983
Ryan Zheng <rzheng@hkn.eecs.berkeley.edu>
2008-08-24 02:19:57 +0000

    rebuild_all works again

--- scripts/generate_model_users.py
+++ scripts/generate_model_users.py
@@ -15,7 +15,7 @@ def main():
     created = {}
     for name in MEMBER_TYPE.names():
         person = Person.objects.create_person(name.title(), "Example", name.lower(), "tcquest+%s@gmail.com" % name.lower(), getattr(MEMBER_TYPE, name))
-        person.profile_picture.save(person.generate_filename(person.username + ".gif"), lion_gif_content)
+        person.save_profile_picture(lion_gif_content)
         created[name.lower()] = person
 
 if __name__=="__main__":
