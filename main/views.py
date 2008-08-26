0fb21fd8da38a3ccd915ca862f721b28fdf64595
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-26 08:43:51 +0000

    landing page content now from flatpage 'landing-page'

--- main/views.py
+++ main/views.py
@@ -2,33 +2,36 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from django.contrib.flatpages.models import FlatPage
 from ajaxwidgets.widgets import JQueryAutoComplete
+from django.utils.safestring import mark_safe
 import datetime
 
 from hkn.event.models import *
 from hkn.event.utils import add_events_metainfo
-from hkn.tutor.views import get_tutor_miniinfo, get_courses_tutored, get_published_assignments
+from hkn.tutor.views import get_tutor_info, get_courses_tutored, get_published_assignments
 from hkn.info import infobox
 
 def main(request):
     d = {}
 
     events = Event.objects.order_by('-start_time').filter_permissions(request.user)[:4]
-    events = add_events_metainfo(request.user, events)
     d['today_events'] = events[:1]
     d['week_events'] = events[1:]
     
     d['day'] = datetime.datetime.now().strftime("%A")
     if d['day'] in ("Saturday", "Sunday"):
         d['day'] = "Monday"
-    d["tutoring_title"] = "%s's Tutoring Schedule" % d['day']
-    schedule, can_tutor, people = get_tutor_miniinfo(get_published_assignments(), day=d['day'])
+    d["tutoring_title"] = "%s Tutoring Schedule" % d['day']
+    schedule, can_tutor, tutors = get_tutor_info(tutoring_days=[d['day']])
     can_tutor = get_courses_tutored(can_tutor)
     d['schedule'] = schedule
-    d['canTutor'] = can_tutor
-    d["preloaded_infoboxes"] = "".join([infobox.tutor(request, "", return_string=True, person=person) for person in people])
+    d['can_tutor'] = can_tutor
+    d["infoboxes"] = infobox.tutors(request, tutors)
     
     xfa = JQueryAutoComplete(source=reverse('course-course-autocomplete'))
     d['exam_files_autocomplete']= xfa.render(name="exam_course")
+
+    d['content'] = mark_safe(FlatPage.objects.get(url="landing-page").content)
 
     return render_to_response("main/main.html", d, context_instance=RequestContext(request))
