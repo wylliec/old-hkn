87d65d970ff4f8e71d1e5e969b0dc99a32e0adc8
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-30 11:01:14 +0000

    added membership_info to person details view

--- info/person.py
+++ info/person.py
@@ -2,6 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
+from django.template.loader import render_to_string
 from hkn.info.models import *
 from hkn.event.rsvp.list import list_for_person_common
 import datetime
@@ -16,5 +17,15 @@ def view(request, person_id):
     d = {}
     d["rsvps"] = person.rsvp_set.order_by("-event__start_time")[:7]
     d["person"] = person    
+    d["membership_info"] = membership_info(person)
     return render_to_response("info/details.html", d, context_instance=RequestContext(request))
 
+def membership_info(person):
+    d = {}
+    d['person'] = person
+    d['officerships'] = person.officership_set.order_by('semester')
+    d['candidate_info'] = person.candidateinfo
+    if len(d['officerships']) == 0 and not person.candidateinfo.initiated:
+        d['no_info'] = True
+    return render_to_string("info/_membership_info.html", d)
+     
