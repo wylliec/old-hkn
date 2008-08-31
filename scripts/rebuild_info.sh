7633af69ee8019aa95f07f8cba6a3d2ab5e97c1a
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-08-31 23:16:38 +0000

    deleted some old scripts

--- scripts/rebuild_info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./create_positions.py
-echo  'c' | ./import_people.py
-echo  'a' | ./import_people.py
-./create_superusers.py
-./import_officership_cached.py
-./import_usernames_cached.py
-
