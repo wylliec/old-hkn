1fe893d85d016273bb77dd0bed5b629184d78f74
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-03 21:08:37 +0000

    yearbook initial incomplete main page

--- yearbook/urls.py
+++ yearbook/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    url(r'^$', 'hkn.yearbook.views.main'),
+    (r'^', include('photologue.urls')),
     )                        
--- yearbook/views.py
+++ yearbook/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+
+from photologue.models import Gallery
+
+def main(request):
+    context = {}
+
+    context['semesters'] = set(Gallery.objects.all().values_list("semester", flat=True))
+
+    return render_to_response('yearbook/main.html', context, context_instance=RequestContext(request))
