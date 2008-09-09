cfd93db9e294d810c417a9191749c04b200a4b6a
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-09 06:32:18 +0000

    fix bug in merging assignments, some (bad?) code to sort assignments

--- info/infobox.py
+++ info/infobox.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from django.core.cache import cache
 from hkn.info.models import Person
 from hkn.tutor.models import get_time_from_slot, get_day_from_slot, make_slot
+from hkn.tutor.constants import TUTORING_DAYS, TUTORING_TIMES
 
 def tutors(request, people):
 	d = {}
@@ -14,21 +15,35 @@ def tutors(request, people):
 		if not tutor_infobox:
 			courses_tutored = ", ".join([ct.course.short_name() for ct in person.cantutor_set.for_current_semester().select_related("course").order_by('course__department_abbr', 'course__integer_number')])
 			d["person"] = person
-			d["assignments"] = merge_assignments(list(person.assignment_set.for_current_semester().latest_version()))
+			d["assignments"] = merge_assignments((list(person.assignment_set.for_current_semester().latest_version())))
 			d["courses"] = courses_tutored
 			tutor_infobox = render_to_string("info/infobox/tutor.html", d)
 			cache.set(cache_key, tutor_infobox, 600)
 		tutors.append(tutor_infobox)
 	return "".join(tutors)
 
+def sort_assignments(assignments):
+	def assignmentSort(x, y):
+		day1 = get_day_from_slot(x.slot)
+		day2 = get_day_from_slot(y.slot)
+		if day1 == day2:
+			slot1 = get_time_from_slot(x.slot)
+			slot2 = get_time_from_slot(y.slot)
+			return list(TUTORING_TIMES).index(slot1) - list(TUTORING_TIMES).index(slot2)
+		return list(TUTORING_DAYS).index(day1) - list(TUTORING_DAYS).index(day2)
+
+	return assignments.sort(cmp=assignmentSort)
+
 def merge_assignments(assignments):
 	if len(assignments) <= 1:
 		return assignments
 
-	slot_pair1 = get_time_from_slot(assignments[0].slot).split('-')
-	slot_pair2 = get_time_from_slot(assignments[1].slot).split('-')
-	if slot_pair1[1] == slot_pair2[0]:
-		assignments[0].slot = make_slot(get_day_from_slot(assignments[0].slot), "-".join([slot_pair1[0] , slot_pair2[1]]))
-		return merge_assignments(assignments[:1] + assignments[2:])
-	else:
-		return assignments[:1] + merge_assignments(assignments[1:])
+	day1 = get_day_from_slot(assignments[0].slot)
+	day2 = get_day_from_slot(assignments[1].slot)
+	if day1 == day2:
+		slot_pair1 = get_time_from_slot(assignments[0].slot).split('-')
+		slot_pair2 = get_time_from_slot(assignments[1].slot).split('-')
+		if slot_pair1[1] == slot_pair2[0]:
+			assignments[0].slot = make_slot(get_day_from_slot(assignments[0].slot), "-".join([slot_pair1[0] , slot_pair2[1]]))
+			return merge_assignments(assignments[:1] + assignments[2:])
+	return assignments[:1] + merge_assignments(assignments[1:])
