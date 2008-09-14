0119675cd36b2e801f594902196a1bf4205ca5fd
Arjun Singh <arjun@hkn.eecs.berkeley.edu>
2008-09-14 01:41:40 +0000

    making event links toggle on landing page

--- static/js/landing.js
+++ static/js/landing.js
@@ -5,47 +5,67 @@ function prepareRow(event_div) {
 }
 
 event_show_info_url = "/event/view/";
-function showInfo(event_id) {
-    event_div = "#event_" + event_id;
-    prepareRow(event_div);
-    
-    url = event_show_info_url + event_id + "/";
-    
-    $.get(url, {}, function(data) {
-        $(event_div).html("<p class='small'>" + data + "</p>");
-    });   
+event_info_div = "#event_" + event_id + "_info";
+event_rsvp_div = "#event_" + event_id + "_rsvp";
+event_rsvp_list_div = "#event_" + event_id + "_rsvp_list";
+
+function showInfo(event_id) { 
+    if($(event_info_div.html() == ""){
+      prepareRow(event_info_div);
+      
+      url = event_show_info_url + event_id + "/";
+      
+      $.get(url, {}, function(data) {
+          $(event_info_div).html("<p class='small'>" + data + "</p>");
+      });  
+    } else {
+      $(event_rsvp_div).hide();
+      $(event_info_div).toggle();
+      $(event_rsvp_list_div).hide();
+    } 
 }
 
 event_rsvp_paragraph_url = "/event/rsvp/list_for_event_paragraph/";
 event_rsvp_list_url = "/event/rsvp/list_event/";
 function showRSVP(event_id) {
-    event_div = "#event_" + event_id;
-    prepareRow(event_div);    
-    
-    url = event_rsvp_paragraph_url + event_id + "/";
-    
-    $.get(url, {}, function(data) {
-        html = [];
-        html.push('<p class="small">');
-        html.push(data);
-        html.push('<br/>');
-        html.push('<a href="' + event_rsvp_list_url + event_id + '/">view all rsvps</a>');        
-        html.push('</p>');
-    
-        $(event_div).html(html.join(''));
-    });   
+    if($(event_rsvp_list_div.html() == ""){
+      prepareRow(event_rsvp_list_div);    
+      
+      url = event_rsvp_paragraph_url + event_id + "/";
+      
+      $.get(url, {}, function(data) {
+          html = [];
+          html.push('<p class="small">');
+          html.push(data);
+          html.push('<br/>');
+          html.push('<a href="' + event_rsvp_list_url + event_rsvp_id + '/">view all rsvps</a>');        
+          html.push('</p>');
+      
+          $(event_rsvp_list_div).html(html.join(''));
+      });
+    } else {
+      $(event_rsvp_div).hide();
+      $(event_info_div).hide();
+      $(event_rsvp_list_div).toggle();      
+    }
 }
 
 rsvp_form_url = "/event/rsvp/edit-ajax/";
 function rsvpForm(event_id) {
-    event_div = "#event_" + event_id;
-    prepareRow(event_div);    
     
-    url = rsvp_form_url + event_id + "/";
-    
-    $.get(url, {}, function(data) {
-        $(event_div).html(data);
-    });   
+    if($(event_rsvp_div.html() == ""){
+      prepareRow(event_rsvp_div);    
+      
+      url = rsvp_form_url + event_id + "/";
+      
+      $.get(url, {}, function(data) {
+          $(event_rsvp_div).html(data);
+      });
+    } else {
+      $(event_rsvp_div).toggle();
+      $(event_info_div).hide();
+      $(event_rsvp_list_div).hide();
+    } 
 }
 
 function rsvpCallback(event_id) {
