6009eec2fdd5e67ef9f06c4f69a8197d38a0a924
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-14 09:02:58 +0000

    fixed indrel admin interface for new forms

--- indrel/admin.py
+++ indrel/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from hkn.indrel.models import InfosessionRegistration
 
 class InfosessionRegistrationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'primaryname', 'primarytitle', 'primaryemail')
+    list_display = ('name', 'contactname', 'contacttitle', 'contactemail')
 
 admin.site.register(InfosessionRegistration, InfosessionRegistrationAdmin)
