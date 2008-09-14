ad5a78423d430b6b5f48502525c01388291af569
Arjun Singh <arjun@hkn.eecs.berkeley.edu>
2008-09-14 09:59:55 +0000

    working callback on event rsvpForm

--- static/js/landing.js
+++ static/js/landing.js
@@ -70,7 +70,7 @@ function rsvpForm(event_id) {
       
       $.get(url, {}, function(data) {
           $(event_rsvp_div).html(data);
-          $('.rsvp_form').ajaxForm(options});
+          $('.rsvp_form').ajaxForm(options);
       });
     }
     
