f1f19c7891dbf1b27db1f28f04df05b8364163cd
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-15 09:43:06 +0000

    consistency update

--- django-common/resume/urls.py
+++ django-common/resume/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
-from resume import views
 
 urlpatterns = patterns('',
-    url(r'^upload/$', views.upload, name="resume-upload"),
+    url(r'^upload/$', 'resume.views.upload', name="resume-upload"),
 )
