1955fb0c42e22a85256e88cf4d9b7b8f5a387520
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-15 04:32:08 +0000

    added tutoring serialization

---
+++ tutor/scripts/common_tutor.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import setup_settings
+from hkn.tutor.models import *
+from hkn.info.models import *
+from course.models import Course
+import pickle
+
+def avail_deserialize(a):
+    return Availability(person=Person.objects.get(username=a[0]), slot=a[1], office=a[2], semester=a[3], preference=a[4]).save()
+
+def avail_serialize(a):
+    return (a.person.username, a.slot, a.office, a.semester.abbr(), a.preference)
+
+def assign_deserialize(a):
+    return Assignment(person=Person.objects.get(username=a[0]), slot=a[1], office=a[2], semester=a[3], semester=a[4], preference=a[5]).save()
+
+def assign_serialize(a):
+    return (a.person.username, a.slot, a.office, a.semester.abbr(), a.version)
+
+def cantutor_deserialize(c):
+    return CanTutor(person=Person.objects.get(username=a[0]), course=Course.objects.ft_query(a[1])[0], semester=a[2], current=a[3]).save()
+
+def cantutor_serialize(c):
+    return (c.person.username, c.course.short_name(space=True), c.semester.abbr(), c.current)
+
+def load_clazz(clazz, d, s):
+    f = file("data/%s.pkl" % clazz.__name__.lower())
+    obs = pickle.load(f)
+    [d(a) for a in obs]
+
+def dump_clazz(clazz, d, s):
+    f = file("data/%s.pkl" % clazz.__name__.lower(), "w")
+    tp = map(s, clazz.objects.all())
+    pickle.dump(tp, f)
+    
+towork = [
+(Availability, avail_deserialize, avail_serialize),
+(Assignment, assign_deserialize, assign_serialize),
+(CanTutor, cantutor_deserialize, cantutor_serialize),
+]
+
+def main(load=False):
+    for w in towork:
+        if load:
+            load_clazz(*w)
+        else:
+            dump_clazz(*w)
---
+++ tutor/scripts/load_tutor.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    import common_tutor
+    common_tutor.main(load=True)
