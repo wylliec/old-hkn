a7e9ac55c5f5796f10118bb8eaf92e602c73b6f9
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-15 10:13:22 +0000

    deleted cand application

--- cand/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-        (r'^confirmed/$', 'hkn.cand.confirm.requirements'),		
-        (r'^initiate/(?P<category>.*)/$', 'hkn.cand.initiate.initiate'),
-        (r'^initiate/$', 'hkn.cand.initiate.initiate'),
-        (r'^initiate_ajax/$', 'hkn.cand.initiate.initiate_ajax'),		
-    )					    
