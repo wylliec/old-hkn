09e1b4f8739b07c13cb7b14543090a460e6a8335
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-09-19 12:47:16 +0000

    updated process_eligibility_list

--- cand/utils.py
+++ cand/utils.py
@@ -5,19 +5,25 @@ from hkn.info.models import *
 from hkn.info.constants import MEMBER_TYPE
 from hkn.info.utils import normalize_email
 
+class EligibilityListException(Exception):
+    pass
+
 def process_eligibility_entry(entry):
     email, first_name, last_name = normalize_email(entry.email_address), entry.first_name, entry.last_name
     persons = Person.objects.filter(Q(email=email) | Q(school_email=email))
     if len(persons) == 0:
         persons = Person.objects.all()
-    elif len(persons) == 1:
+    elif len(persons) > 1:
+        import logging
+        logging.getLogger('special.exceptions').error("Multiple people matched for email %s: %s" % (email, str(persons)))
+        persons = [max(persons, key=lambda person: person.member_type)]
+
+    if len(persons) == 1:
         print "Matched email for %s" % email
         if persons[0].member_type >= MEMBER_TYPE.MEMBER:
             return (persons[0], "MEMBER")
         else:
             return (persons[0], "CANDIDATE")
-    elif len(persons) > 1:
-        assert(False)
 
     persons = persons.filter(Q(Q(first_name=first_name)|Q(realfirst=first_name))&Q(last_name=last_name))
     if len(persons) == 0:
@@ -31,7 +37,7 @@ def process_eligibility_entry(entry):
             return (persons[0], "MAYBE_CAND")
     else:
         print "-- Too many name matches for %s %s" % (first_name, last_name)
-        return (None, "MAYBE_MEMBER")
+        return (min(persons, key=lambda person:person.pk), "MAYBE_MEMBER")
     return (None, None)
 
 def process_eligibility_list():
