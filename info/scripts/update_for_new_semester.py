ec22b103e983d8ab4f1d43bbb7a4387f70568779
Hisham Zarka <hzarka@hkn.eecs.berkeley.edu>
2008-12-09 09:32:20 +0000

    fixed update for semester script

--- info/scripts/update_for_new_semester.py
+++ info/scripts/update_for_new_semester.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import setup_settings
 
+from hkn.info.models import Person
+
 def main():
     for p in Person.objects.all():
         p.reconcile_status()
         p.save()
     
+
+if __name__ == "__main__":
+    main()
