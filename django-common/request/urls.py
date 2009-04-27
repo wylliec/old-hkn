bf001ce2c445261b49fb37def979a65befe8cd6a
Gilbert Chou <gkchou@hkn.eecs.berkeley.edu>
2009-04-27 06:47:10 +0000

    show inactive requests

--- django-common/request/urls.py
+++ django-common/request/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
         url(r'^list/(?P<category>.*)/$', 'request.list.list_requests', name="request-list-category"),
         url(r'^list/$', 'request.list.list_requests', {"category" : "actives"}, name="request-list"),
+        url(r'^list_inactive/', 'request.list.list_requests', {"category" : "inactives"}, name="request-list-inactive"),
         url(r'^list_requests_confirm_ajax/$', 'request.list.list_requests_confirm_ajax', name="request-ajax-confirm"),
     )                        
