bba5c073d11c9d14000b0fe9188a1f00c7248664
Ibrahim Awwal <ibrahima@hkn.eecs.berkeley.edu>
2009-12-23 01:21:43 -0800

    Added a doc comment to the hkn.gcal.views.ics view. This is just to test whether automatic pulls from gitosis are working.

--- gcal/views.py
+++ gcal/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render_to_response
 from hkn.event.models import Event
 import datetime
 def ics(request):
+    """
+    This view generates an iCal file from the hkn calendar.
+    """
     events = Event.public.all()
     r = render_to_response("calendar/hkn.ics", {'events' : events, 'today':datetime.datetime.now()}, mimetype="text/calendar")
     r['Content-Disposition'] = 'inline; filename=hkn.ics'
