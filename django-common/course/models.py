ecb65e8e74324e829be93e26639b91f701d5a8ee
Richard Xia <rxia@eecs.berkeley.edu>
2010-01-18 15:56:55 -0800

    Added needs_survey attribute to Klass.

--- django-common/course/models.py
+++ django-common/course/models.py
@@ -263,17 +263,17 @@ class Klass(models.Model):
     newsgroup = models.CharField(max_length = 100)
     """ The klass newsgroup """
 
-    #surveyed = models.BooleanField()
+    needs_survey = models.BooleanField()
     """ whether the course needs course surveys """
 
     #enrolled = models.IntegerField()
-    """ number of people enrolled """
+    #""" number of people enrolled """
 
     #time = models.CharField(max_length = 100)
-    """ time and day for the lecture """
+    #""" time and day for the lecture """
 
     #location = models.CharField(max_length = 100)
-    """ location of the lecture """
+    #""" location of the lecture """
 
     cached_instructor_names = models.CharField(max_length = 200, null=True)
     """ cached instructor names """
